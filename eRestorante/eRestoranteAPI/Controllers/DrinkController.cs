343d23242e8bcbab6a6a7e0246500e2f68698e85
BerunBiH <berun.agic@hotmail.com>
2024-05-29 12:53:36 +0200

    C20 - Added Authorization to API
    Added Authorization to API endpoints when you want to create or update a dish or drink.

--- eRestorante/eRestoranteAPI/Controllers/DrinkController.cs
+++ eRestorante/eRestoranteAPI/Controllers/DrinkController.cs
@@ -2,6 +2,7 @@ using eRestorante.Models.Requests;
 using eRestorante.Models.SearchObjects;
 using eRestorante.Services.Database;
 using eRestorante.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
@@ -12,6 +13,18 @@ namespace eRestoranteAPI.Controllers
     {
         public DrinkController(ILogger<BaseController<eRestorante.Models.Model.Drink, DrinkSearchObject>> logger, IDrinkService service) : base(logger, service)
         {
+        }
+
+        [Authorize(Roles = "Menedzer, Šanker")]
+        public override Task<eRestorante.Models.Model.Drink> Insert([FromBody] DrinkInsertRequest insert)
+        {
+            return base.Insert(insert);
+        }
+
+        [Authorize(Roles = "Menedzer, Šanker")]
+        public override Task<eRestorante.Models.Model.Drink> Update(int id, [FromBody] DrinkUpdateRequest update)
+        {
+            return base.Update(id, update);
         }
     }
 }
